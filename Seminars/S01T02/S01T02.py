12b4f37eb02629647f0b0fabe5138b70ca3a6cc6
Alexander_G <alexander.4ephb@gmail.com>
2022-11-26 08:08:30 +1000

    S01T02 Added

---
+++ Seminars/S01T02/S01T02.py
@@ -0,0 +1,20 @@
+"""
+Базовое задание:
+2. Пользователь вводит время в секундах. Программа переводит время в часы,
+минуты и секунды и выводит в формате чч:мм:сс
+"""
+
+input_sec = int(input('Введите время в секундах: '))
+
+hours = input_sec // 3600
+minutes = int((input_sec % 3600) / 60)
+seconds = int((input_sec % 3600) % 60)
+
+if hours < 10:
+    hours = str(f'0{hours}')
+if minutes < 10:
+    minutes = str(f'0{minutes}')
+if seconds < 10:
+    seconds = str(f'0{seconds}')
+
+print(f"{hours}:{minutes}:{seconds}")
